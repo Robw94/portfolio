7cc9b428f51ad5346e09d63220d058c616fd4c60
Rob <46281347+Robw94@users.noreply.github.com>
2021-05-03 17:13:33 +0100

    Initial Commit

---
+++ robwebb94-github-io/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioComponent,
+    IntroductionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
